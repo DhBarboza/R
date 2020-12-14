77683b9847b0d250ff737146aa0dca30d4860fe6
Douglas Henrique <dev.dhbarboza@gmail.com>
2020-12-14 20:17:19 -0300

    data types overview

---
+++ data-types.R
@@ -0,0 +1,20 @@
+# Character: "a", "Hello, Programmer"
+character <- 'Hello, Programmer'
+
+#Numeric: 3, 17.2, -10
+number <- 10
+
+# Integer: 3L, 10L
+integer <- 10L
+
+#Logical: TRUE, FALSE, T, F
+boolean <- TRUE
+
+# Vectors:
+vector <- c(10, 20, 30)
+
+# </ Special Values >
+#inf: infinity
+infinity <- Inf
+
+Not_a_Number <- NaN
